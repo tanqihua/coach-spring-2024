fe027df57fdab315fa9e979b0198689dedc91c01
掉链子了咯 <74001180+tanqihua@users.noreply.github.com>
2024-02-15 18:58:31 +0800

    replace hires

--- src/2d/bootstrap.js
+++ src/2d/bootstrap.js
@@ -41,11 +41,11 @@ export class Bootstrap extends Phaser.Scene {
       frameWidth: 400,
       frameHeight: 400,
     });
-    this.load.video("purpleVideo", "/2d/Purple_FullVideo.mp4");
-    this.load.video("yellowVideo", "/2d/Yellow_FullVideo.mp4");
-    this.load.video("blackVideo", "/2d/Black_FullVideo.mp4");
-    this.load.video("tyeDyeVideo", "/2d/TyeDye_FullVideo.mp4");
-    this.load.video("denimVideo", "/2d/Denim_FullVideo.mp4");
+    this.load.video("purpleVideo", "/2d/Purple_FullVideo_High.mp4");
+    this.load.video("yellowVideo", "/2d/Yellow_FullVideo_High.mp4");
+    this.load.video("blackVideo", "/2d/Black_FullVideo_High.mp4");
+    this.load.video("tyeDyeVideo", "/2d/TyeDye_FullVideo_High.mp4");
+    this.load.video("denimVideo", "/2d/Denim_FullVideo_High.mp4");
 
     // laod audio
     this.load.audio("infration", "/PowerMode1.mp3");
